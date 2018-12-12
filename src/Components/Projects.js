d16fef08f17b41525855d09d222c4a7f46d8f87e
danchamorro <dan@danchamorro.com>
2018-12-11 19:21:25 -0500

    Projects Grid

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -21,31 +21,86 @@ class Projects extends Component {
   toggleCategories = () => {
     if (this.state.activeTab === 0) {
       return (
-        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-          <CardTitle
-            style={{
-              color: "#fff",
-              height: "176px",
-              background:
-                "url(https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png) center / cover"
-            }}
-          >
-            React Project #1
-          </CardTitle>
-          <CardText>
-            Vestibulum ac diam sit amet quam vehicula elementum sed sit amet
-            dui. Cras ultricies ligula sed magna dictum porta. Sed porttitor
-            lectus nibh. Vivamus magna justo, lacinia eget consectetur sed,
-            convallis at tellus.
-          </CardText>
-          <CardActions border>
-            <Button colored>GitHub</Button>
-            <Button colored>Live Demo</Button>
-          </CardActions>
-          <CardMenu style={{ color: "#fff" }}>
-            <IconButton name="share" />
-          </CardMenu>
-        </Card>
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png) center / cover"
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>
+              Vestibulum ac diam sit amet quam vehicula elementum sed sit amet
+              dui. Cras ultricies ligula sed magna dictum porta. Sed porttitor
+              lectus nibh. Vivamus magna justo, lacinia eget consectetur sed,
+              convallis at tellus.
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          {/* Project 2 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png) center / cover"
+              }}
+            >
+              React Project #2
+            </CardTitle>
+            <CardText>
+              Vestibulum ac diam sit amet quam vehicula elementum sed sit amet
+              dui. Cras ultricies ligula sed magna dictum porta. Sed porttitor
+              lectus nibh. Vivamus magna justo, lacinia eget consectetur sed,
+              convallis at tellus.
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          {/* Project 3 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png) center / cover"
+              }}
+            >
+              React Project #3
+            </CardTitle>
+            <CardText>
+              Vestibulum ac diam sit amet quam vehicula elementum sed sit amet
+              dui. Cras ultricies ligula sed magna dictum porta. Sed porttitor
+              lectus nibh. Vivamus magna justo, lacinia eget consectetur sed,
+              convallis at tellus.
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
       );
     } else if (this.state.activeTab === 1) {
       return (
@@ -82,13 +137,11 @@ class Projects extends Component {
           <Tab>MongoDB</Tab>
         </Tabs>
 
-        <section className="projects-grid">
-          <Grid className="projects-grid">
-            <Cell col={12}>
-              <div className="content">{this.toggleCategories()}</div>
-            </Cell>
-          </Grid>
-        </section>
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
